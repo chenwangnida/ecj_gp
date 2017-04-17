0064f628f28e20f1e198fadc8452a865c91561b9
Jeffrey Wong <chenwang@Chens-MacBook-Pro.local>
2017-04-17 14:54:59 +1200

    update

---
+++ src/app/tutorial4/Initilializer.java
@@ -0,0 +1,16 @@
+package app.tutorial4;
+
+import ec.EvolutionState;
+import ec.gp.GPInitializer;
+import ec.util.Parameter;
+
+public class Initilializer extends GPInitializer {
+
+	@Override
+	public void setup(EvolutionState state, Parameter base) {
+		// TODO Auto-generated method stub
+		super.setup(state, base);
+		
+	}
+
+}
